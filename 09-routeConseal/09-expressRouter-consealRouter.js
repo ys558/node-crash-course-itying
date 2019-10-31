f8d7b51c5b3a145ed789af8e074af8dbce08fdf4
ys558 <yuyi.gz@163.com>
2019-10-31 18:31:58 +0800

    完成模仿express封装路由ejs模板渲染

--- 09-routeConseal/09-expressRouter-consealRouter.js
+++ 09-routeConseal/09-expressRouter-consealRouter.js
@@ -1,15 +1,48 @@
 // 封装09-routeConseal\09-consealRouter-Express-inner-logic-2.js
 const url = require('url')
 
+/* 封装http.createServer里的经常写的res.writeHead()和res.end()部分，绑定res的send方法*/ 
+Respond = (res) =>{
+    res.send = (data) =>{
+        res.writeHead(200, { 'Content-Type': 'text/html'})
+        res.end(data)
+        console.log(`success!!!`);
+    }
+}
+
+
 const app= ()=>{
     const G = this
+    // 处理get和post请求：
+    this._get = {}
+    this._post = {}
+
     const app = (req, res) => {
+        Respond(res)
+
         let path=url.parse(req.url).path
 
+        // 获取路由：
         if(!path.endsWith('/')) path=path+'/'
-    
-        if(G[path]) { 
-            G[path](req, res)
+        
+        // 获取请求方式：
+        const method=req.method.toLowerCase()
+        
+
+        if(G[`_${method}`][path]) {
+            if(method=='post'){
+                // 执行post请求：用res.on方法拿：
+                let postStr = ''
+                req.on('data', (chunk)=> postStr += chunk)
+                req.on('end', (err, chunk) => {
+                    console.log(postStr)
+                    /*表示拿到post的值*/ 
+                    req.body=postStr
+                    G[`_${method}`][path](req, res)
+                })
+            }else{
+                G[`_${method}`][path](req, res)
+            }
         }else{
             res.end(`<head><meta charset="utf-8"/></head><body><h2>路由不存在</h2></body>`)
         }
@@ -17,7 +50,12 @@ const app= ()=>{
     app.get = (str, callback) => {
         if(!str.endsWith('/'))  str=str+'/'
         if(!str.startsWith('/'))  str='/'+str
-        G[str] = callback
+        G._get[str] = callback
+    }
+    app.post = (str, callback) => {
+        if(!str.endsWith('/'))  str=str+'/'
+        if(!str.startsWith('/'))  str='/'+str
+        G._post[str] = callback
     }
     return app
 }
--- 09-routeConseal/09-expressRouter-inUse.js
+++ 09-routeConseal/09-expressRouter-inUse.js
@@ -1,9 +1,26 @@
 const app = require('./09-expressRouter-consealRouter')
 const http = require('http')
+const ejs = require('ejs')
+
 
 http.createServer(app).listen(5010, ()=>{console.log(`listen on port 5010`)})
 
 app.get('/login', (req, res)=>{
-    res.end(`<head><meta charset="utf8"></meta></head><body><h2>login route</h2></body>`)
-    console.log(`login router success`)
+    ejs.renderFile('./09-expressRouter-inUse-loginForm.ejs', {}, (err, data) =>{
+        res.send(data)
+    })
 })
+
+app.post('/dologin', (req, res)=>{
+    console.log(req.body)
+    res.send(`<head><meta charset="utf-8"/></head><script>alert('登录成功');history.back();</script>`)
+})
+
+
+app.get('/register', (req, res)=>{
+    ejs.renderFile('./09-expressRouter-inUse-registerForm.ejs', {}, (err, data)=>{
+        res.send(data)
+    })
+    
+})
+
